688ae49fedc000f2ec6db5d77430fded6e7aa86f
Okan Kaplan <okan149@gmail.com>
2024-06-28 20:36:57 +0200

    styling styling startscreen and burgermenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { StartscreenComponent } from './startscreen/startscreen.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, StartscreenComponent],
+  imports: [CommonModule, RouterOutlet, StartscreenComponent, FooterComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+width: { [klass: string]: any; }|null|undefined;
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
-
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatMenuModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  menuActive: boolean = false;
+  currentFrame: number = 0;
+  frames: string[] = [
+    './../../../assets/img/burgermenu1.png',
+    './../../../assets/img/burgermenu2.png',
+    './../../../assets/img/burgermenu3.png',
+    './../../../assets/img/burgermenu4.png',
+    './../../../assets/img/burgermenu5.png',
+  ];
+
+  toggleMenu() {
+    if (!this.menuActive) {
+      this.playAnimation(true);
+    } else {
+      this.playAnimation(false);
+    }
+  }
+
+  playAnimation(opening: boolean) {
+    const interval = 150;
+    let frameIndex = 0;
+
+    const animation = setInterval(() => {
+      this.currentFrame = opening ? frameIndex : this.frames.length - 1 - frameIndex;
+      frameIndex++;
+      if (frameIndex === this.frames.length) {
+        clearInterval(animation);
+        this.menuActive = opening;
+        if (!opening) {
+          this.currentFrame = 0; 
+        }
+      }
+    }, interval);
+  }
+
+  getCurrentFrame() {
+    return this.frames[this.currentFrame];
+  }
 }
