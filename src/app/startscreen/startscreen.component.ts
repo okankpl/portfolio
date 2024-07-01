40a3fa8ae8dd04b30c1b64b79caaf744bf5d93d7
Okan Kaplan <okan149@gmail.com>
2024-07-01 22:02:19 +0200

    styling scroll arrow animaiton

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -8,27 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './startscreen.component.scss'
 })
 export class StartscreenComponent {
-  animationTimeout: any;
-  animationInterval: any;
-
-  startAnimation() {
-    const scrollImgElement = document.getElementById('scroll-img') as HTMLImageElement;
-    if (scrollImgElement) {
-      let frame = 1;
-      this.animationInterval = setInterval(() => {
-        frame = frame < 4 ? frame + 1 : 1;
-        scrollImgElement.src = `./../../assets/img/scrolldown${frame}.png`;
-      }, 250); 
-    }
-  }
-
-  stopAnimation() {
-    clearInterval(this.animationInterval);
-    const scrollImgElement = document.getElementById('scroll-img') as HTMLImageElement;
-    if (scrollImgElement) {
-      this.animationTimeout = setTimeout(() => {
-        scrollImgElement.src = './../../assets/img/scrolldown1.png';
-      }, 500); 
-    }
-  }
+ 
 }
