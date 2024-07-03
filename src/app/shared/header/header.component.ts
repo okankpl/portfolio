9c66f6274d96af0b2150a8856afe33d6f039470f
Okan Kaplan <okan149@gmail.com>
2024-07-03 20:31:15 +0200

    styled the links in the header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,30 +1,66 @@
-import { Component, Renderer2, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [MatMenuModule, MatButtonModule],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('toggleOverlay', [
+      state('inactive', style({
+        height: '0',
+        overflow: 'hidden'
+      })),
+      state('active', style({
+        height: '92vh'
+      })),
+      transition('inactive <=> active', [
+        animate('0.3s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class HeaderComponent {
   menuActive: boolean = false;
-
-  constructor(private renderer: Renderer2, private el: ElementRef) {}
+  currentFrame: number = 1;
+  animationInterval: any;
+  isAnimating: boolean = false; // Neue Variable, um den Animationszustand zu speichern
 
   toggleMenu() {
-    const overlay = this.el.nativeElement.querySelector('.overlay');
-    this.menuActive = !this.menuActive;
-    if (this.menuActive) {
-      this.renderer.addClass(overlay, 'active');
+    if (this.isAnimating) return; // Blockiere, wenn eine Animation läuft
+
+    this.isAnimating = true; // Setze den Animationszustand auf aktiv
+
+    if (!this.menuActive) {
+      this.startAnimation(1, 5, () => {
+        this.menuActive = true;
+        this.isAnimating = false; // Setze den Animationszustand auf beendet
+      });
     } else {
-      this.renderer.removeClass(overlay, 'active');
+      this.startAnimation(5, 1, () => {
+        this.menuActive = false;
+        this.isAnimating = false; // Setze den Animationszustand auf beendet
+      });
     }
   }
 
+  startAnimation(start: number, end: number, callback: () => void) {
+    let step = start < end ? 1 : -1;
+    this.currentFrame = start;
+    this.animationInterval = setInterval(() => {
+      this.currentFrame += step;
+      if (this.currentFrame === end) {
+        clearInterval(this.animationInterval);
+        callback();
+      }
+    }, 100); 
+  }
+
   getCurrentFrame() {
-    return this.menuActive ? './../../assets/img/burgermenu5.png' : './../../assets/img/burgermenu1.png';
+    return `./../../assets/img/burgermenu${this.currentFrame}.png`;
   }
 }
