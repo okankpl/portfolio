4e2bb31165b0faeaf2ef6e7fd52f41e8e804cff7
Okan Kaplan <okan149@gmail.com>
2024-07-04 19:09:01 +0200

    finished styling of header and startscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { StartscreenComponent } from './startscreen/startscreen.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, StartscreenComponent, FooterComponent,HeaderComponent],
+  imports: [CommonModule, RouterOutlet, StartscreenComponent, FooterComponent,HeaderComponent,AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
