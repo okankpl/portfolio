cb10d0faa4f9ad84e751b79749b92891b0fd5a0c
Okan Kaplan <okan149@gmail.com>
2024-07-11 18:43:27 +0200

    adding php email data

--- src/app/shared/contactform/contactform.component.ts
+++ src/app/shared/contactform/contactform.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contactform',
@@ -11,13 +12,44 @@ import { FormsModule } from '@angular/forms';
 
 
 export class ContactformComponent {
+
+  http = inject(HttpClient); 
+
   contactData = {
     name: "",
     email: "",
     message: ""
   }
 
-  onSubmit() {
-    console.log(this.contactData);
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://okan-kaplan.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
   }
 }
