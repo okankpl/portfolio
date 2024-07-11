0a731cf11f7bfd22df94ca5688aa0c776ba1d4cd
Okan Kaplan <okan149@gmail.com>
2024-07-11 17:40:02 +0200

    styling footer and contactform

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactformComponent } from '../contactform/contactform.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [ContactformComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
