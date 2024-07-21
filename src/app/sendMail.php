a240234eb8ea62f49489ecd3e8a20b75e0a73c6b
Okan Kaplan <okan149@gmail.com>
2024-07-21 15:21:22 +0200

    changing the linked link

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'okan149@gmail.com';  
+            $recipient = 'contact@okan-kaplan.de';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
